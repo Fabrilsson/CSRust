cdd69a100cea514b9d03e83a1f9c862b8e5ed17c
fabrilsson <fabriciosperafico@outlook.com>
2021-10-03 00:51:05 -0300

    more parsing

---
+++ ApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemoryTestWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet("bigstring")]
+        public ActionResult<string> Get()
+        {
+            return new String('x', 10 * 1024);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ extern crate pest;
 extern crate pest_derive;
 
 use pest::Parser;
+use std::fs;
 
 #[derive(Parser)]
 #[grammar = "CS.pest"]
 pub struct CSParser;
 
 fn main() {
-    let successful_parse = CSParser::parse(Rule::main, "public main () { if (asdasd > asdasd) }");
+    let contents = fs::read_to_string("ApiController.cs")
+    .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
+    let successful_parse = CSParser::parse(Rule::main, "using System; using Microsoft.AspNetCore.Mvc; namespace MemoryTestWebApi.Controllers { [TesteAttribute] [TesteAttribute2(\"asdasd\")] public main () { if (asdasd > asdasd) } }");
     println!("{:?}", successful_parse);
 }
