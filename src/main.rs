cef45d1523b83db990725ee49dc07e0fd9cf0160
Fabrilsson <fabriciosperafico@outlook.com>
2022-03-24 22:53:41 -0300

    commit to save method

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate pest_derive;
 
 use pest::Parser;
 use std::fs;
+use std::fs::File;
+use pest::iterators::Pair;
 
 #[derive(Parser)]
 #[grammar = "CS.pest"]
@@ -13,40 +15,36 @@ fn main() {
     let contents = fs::read_to_string("ApiController.cs")
     .expect("Something went wrong reading the file");
 
-    let asdasd = contents.replace("\u{feff}", "");
+    let text = contents.replace("\u{feff}", "");
 
     println!("With text:\n{}", contents);
 
-    let successful_parse = CSParser::parse(Rule::main, &asdasd).unwrap_or_else(|e| panic!("{}", e));
+    let successful_parse = CSParser::parse(Rule::main, &text).unwrap_or_else(|e| panic!("{}", e));
 
     println!("{:?}", successful_parse);
-
-    let mut count = 1;
 
     for pair in successful_parse {
         println!("Rule:    {:?}", pair.as_rule());
         println!("Span:    {:?}", pair.as_span());
         println!("Text:    {}", pair.as_str());
 
-        for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::using_code_block => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::namespace_code_block => println!("teste:  {}", inner_pair.as_str()),
-                _ => unreachable!()
-            };
+        match_pairs(pair);
+    }
+}
 
-            if count == 1 || count == 2 {
-                count = count + 1;
+fn match_pairs(iter: Pair<Rule>) {
 
-                continue;
-            }
-
-            for elem in inner_pair.into_inner() {
-                match elem.as_rule() {
-                    Rule::namespace_key_work => println!("teste2:  {}", elem.as_str()),
-                    _ => unreachable!()
-                };
-            }
-        }
+    for elem in iter.into_inner() {
+        match elem.as_rule() {
+            Rule::using_code_block => println!("Letter:  {}", elem.as_str()),
+            Rule::namespace_code_block => match_pairs(elem),
+            Rule::namespace_key_work => println!("teste2:  {}", elem.as_str()),
+            Rule::identifier => println!("teste2:  {}", elem.as_str()),
+            Rule::left_bracers => println!("teste2:  {}", elem.as_str()),
+            Rule::class_code => match_pairs(elem),
+            Rule::attribute => println!("teste2:  {}", elem.as_str()),
+            Rule::public_key_word => println!("teste2:  {}", elem.as_str()),
+            _ => unreachable!()
+        };
     }
 }
